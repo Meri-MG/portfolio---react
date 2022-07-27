01ba137fef1a296d43c93a231e7bf5bd6a0fd2e6
Meri <mgogi13@freeuni.edu.ge>
2022-07-27 22:09:23 +0400

    fix: update some links, cv, readme, styles

--- README.md
+++ README.md
@@ -60,4 +60,4 @@
 Give a ⭐ if you like this project!
 
 # Acknowledgments
-Thanks to [EGATOR](https://www.youtube.com/watch?v=G-Cr00UYokU&list=WL&index=55&t=1845s) tutotial!
+Thanks to [EGATOR](https://www.youtube.com/watch?v=G-Cr00UYokU&list=WL&index=55&t=1845s) tutorial!
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -6,7 +6,6 @@ import IMG4 from '../../assets/fake.png';
 import IMG5 from '../../assets/news.png';
 import IMG6 from '../../assets/math.png';
 
-
 import './portfolio.css';
 
 const Portfolio = () => {
@@ -15,6 +14,9 @@ const Portfolio = () => {
       id: 1,
       title: 'Bookstore',
       img: IMG1,
+      description:
+        'BookStore app that allows users to store books and update progress',
+      technologies: 'React | Redux | Ruby on Rails',
       link: 'https://melodic-boba-111583.netlify.app/',
       github: 'https://github.com/Meri-MG/bookstore---react',
     },
@@ -22,13 +24,18 @@ const Portfolio = () => {
       id: 2,
       title: 'FakeStore',
       img: IMG4,
-      link: 'https://angry-yalow-17c6ab.netlify.app/',
-      github: 'https://github.com/Meri-MG/metrics-webapp---react',
+      description:
+        'FakeStore app that provides information about available products and their details',
+      technologies: 'React | Redux',
+      link: 'https://fakestore-metrics.netlify.app/',
+      github: 'https://github.com/Meri-MG/Fakestore---react',
     },
     {
       id: 3,
       title: 'To-Do-App',
       img: IMG2,
+      description: 'User friendly app to plan your day and manage tasks',
+      technologies: 'JavaScript | Webpack',
       link: 'https://meri-mg.github.io/To-Do-List/dist/',
       github: 'https://github.com/Meri-MG/To-Do-List',
     },
@@ -36,6 +43,9 @@ const Portfolio = () => {
       id: 4,
       title: 'Shelter',
       img: IMG3,
+      description:
+        'Fully responsive interactive website built based on Figma design',
+      technologies: 'JavaScript | CSS',
       link: 'https://meri-mg.github.io/shelter/pages/main/index.html',
       github: 'https://github.com/Meri-MG/shelter',
     },
@@ -43,6 +53,9 @@ const Portfolio = () => {
       id: 5,
       title: 'World News',
       img: IMG5,
+      description:
+        'Fully responsive interactive website built based on Adobe XD design',
+      technologies: 'JavaScript | CSS',
       link: 'https://meri-mg.github.io/Unilab-world-news/',
       github: 'https://github.com/Meri-MG/Unilab-world-news',
     },
@@ -50,11 +63,13 @@ const Portfolio = () => {
       id: 6,
       title: 'Math Resource',
       img: IMG6,
+      description:
+        'Real-world group project which is still in progress and will provide educational platform for future young developers',
+      technologies: 'JavaScript | Scss | Python',
       link: 'https://lukinoo.github.io/math-resource/',
       github: 'https://github.com/lukinoo/math-resource',
     },
   ];
-  
 
   return (
     <section id="portfolio">
@@ -64,19 +79,37 @@ const Portfolio = () => {
       <div className="container portfolio__container">
         {soloProjects.map((pro) => (
           <article className="portfolio__item" key={pro.id}>
-          <div className="portfolio__item-image">
-            <img src={pro.img} alt={pro.title} />
-          </div>
-          <h3>{pro.title}</h3>
-          <div className="portfolio__item-cta">
-            <a href={pro.github} className="btn">GitHub</a>
-            <a href={pro.link} className="btn btn-primary">Live Demo</a>
-          </div>
-        </article>
-        ))}   
+            <div className="portfolio__item-image">
+              <img src={pro.img} alt={pro.title} />
+            </div>
+            <div className="portfolio__item-content">
+              <h3>{pro.title}</h3>
+              <p>{pro.description}</p>
+              <p>{pro.technologies}</p>
+            </div>
+            <div className="portfolio__item-cta">
+              <a
+                href={pro.github}
+                target="_blank"
+                className="btn"
+                rel="noreferrer"
+              >
+                GitHub
+              </a>
+              <a
+                href={pro.link}
+                target="_blank"
+                className="btn btn-primary"
+                rel="noreferrer"
+              >
+                Live Demo
+              </a>
+            </div>
+          </article>
+        ))}
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Portfolio
+export default Portfolio;
