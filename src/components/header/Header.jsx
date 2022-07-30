9300b3cd507017959497f8a60f4ed99cad60c152
Meri <mgogi13@freeuni.edu.ge>
2022-07-30 17:23:15 +0400

    fix the title

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -9,13 +9,15 @@ const Header = () => {
       <div className="container header__container">
         <h5>Hello I'm</h5>
         <h1>Meri Gogichashvili</h1>
-        <h5 className="text-light">Front-end Developer</h5>
+        <h5 className="text-light">Full-stack Developer</h5>
         <CTA />
-        <a href="#contact" className="scroll__down">Scroll Down</a>
+        <a href="#contact" className="scroll__down">
+          Scroll Down
+        </a>
         <HeaderSocials />
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
