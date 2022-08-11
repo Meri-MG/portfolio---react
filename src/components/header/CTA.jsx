a77faccb9bdb6981a64142514204f0f49c3d84a1
Meri <mgogi13@freeuni.edu.ge>
2022-08-11 11:35:58 +0400

    fix: update cv

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
-import CV from '../../assets/cv.pdf';
+import CV from '../../assets/cv_meri_gogichashvili.pdf';
 
 const CTA = () => {
   return (
     <div className="cta">
-      <a href={CV} download className="btn">Download CV</a>
-      <a href="#contact" className="btn btn-primary">Let's talk</a>
+      <a href={CV} download className="btn">
+        Download CV
+      </a>
+      <a href="#contact" className="btn btn-primary">
+        Let's talk
+      </a>
     </div>
-  )
-}
+  );
+};
 
-export default CTA
+export default CTA;
